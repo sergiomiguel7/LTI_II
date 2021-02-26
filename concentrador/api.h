8414e4152caa2477870abdcfa52f2b5ccd26f54c
Sergio Carvalho <sergiompc12@gmail.com>
2021-02-26 19:48:18 +0000

    Concentrador update

---
+++ concentrador/api.h
@@ -0,0 +1,13 @@
+
+#define ADDR "127.0.0.1"
+#define PORT 8000
+#define CONFIG_FILE "config.txt"
+
+
+//vars para socket
+struct sockaddr_in servAddr, cliaddr;
+int sockfd;
+
+//socket
+int sockManager;
+
---
+++ concentrador/concentrador.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include "api.h"
+
+int serverFd
+
+int main(){
+
+    sockManager = connectSocket();
+    if(sockManager > 0){
+        printf("Socket connected");
+    }
+
+}
+
+
+/*
+*   function to create a ipv4 socket connection
+*   returns the socket file descriptor
+*/
+int connectSocket(){
+    int sock = 0;
+
+    //try to create a new default socket with ipv4 domain
+    if(sock = socket(AF_INET, SOCK_STREAM, 0) < 0){
+        printf("Socket creation error \n");
+		return -1;
+    }
+
+    memset(&servAddr, '0', sizeof(servAddr));
+
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htons(PORT);
+
+    // Convert IPv4 address from text to binary form to config socket
+	if (inet_pton(AF_INET, ADDR, &servAddr.sin_addr) <= 0)
+	{
+		printf("\nInvalid address/ Address not supported \n");
+		return -1;
+	}
+    //servAddr configured with sucess, now connect socket
+	if (connect(sock, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
+	{
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+
+	return sock;
+}
