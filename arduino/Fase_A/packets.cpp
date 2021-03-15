cdd43636d846754266c51118ed66836c6c79be2b
fariapedro <fariapedro27@gmail.com>
2021-03-15 16:47:45 +0000

    Packets.cpp Leitura do pacote Start

--- arduino/Fase_A/packets.cpp
+++ arduino/Fase_A/packets.cpp
@@ -1,14 +1,29 @@
 #include "packets.h"
 #include "Arduino.h"
+#include "api.h"
 
 /*
-packet[] -> Pacote á qual vai ser adiciona o value
-value -> conteudo do  tipo char que irá ser adicionado ao pacote
-pos -> apontador para a proxima posição livre no pacote
+  packet[] -> Pacote á qual vai ser adiciona o value
+  value -> conteudo do  tipo char que irá ser adicionado ao pacote
+  pos -> apontador para a proxima posição livre no pacote
 */
 void addChar(char packet[], char value, int pos) {
   if (pos < 1028) {
     packet[pos] = value;
     pos++;
   }
+}
+
+
+/*
+  tsp -> Timestamp no momento em que se recebe o pacote Start
+  pm -> Periodo de leitura de dados do sensor
+  pa -> Periodo entre amostras
+  na -> Número total de amostras
+*/
+void startPacket(char packet[], uint32_t tsp, uint32_t pm , uint32_t pa, int na ) {
+  tsp = join32((char *)&packet[1]);
+  pm = join32((char *)&packet[5]);
+  pa = join32((char *)&packet[9]);
+  na = (int)packet[13];
 }
--- arduino/Fase_A/packets.h
+++ arduino/Fase_A/packets.h
@@ -1,3 +1,5 @@
 #include "Arduino.h"
 
 void addChar(char packet[], char value, int pos);
+
+void startPacket(char packet[], uint32_t tsp, uint32_t pm , uint32_t pa, int na );
