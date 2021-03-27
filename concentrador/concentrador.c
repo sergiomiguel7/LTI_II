2710014f67c18b5dfc346ce4970b9a58f65f0454
Sergio Carvalho <sergiompc12@gmail.com>
2021-03-27 19:07:58 +0000

    write on files

--- concentrador/concentrador.c
+++ concentrador/concentrador.c
@@ -21,66 +21,6 @@ int buildStartPacket(char *str, int index);
 int buildStopPacket(char *str, uint8_t stopCode);
 void receiveData(char *readBuf);
 
-int main()
-{
-    signal(SIGINT, handler);
-    configuredPorts = 0;
-
-    char bufWrite[1024];
-    char bufRead[1024];
-
-    //  int coms = comEnumerate();
-    readConfigFile();
-    openSerial();
-    handleBegin(bufWrite);
-    receiveData(bufRead);
-}
-
-/**
- * @param readBuf - char buffer
- * handles incoming data from sensors
- * DATAx => |0 -> TYPE | 1 -> ISS | 2-5 -> TSP | 6 -> TGM | 7... -> VALi| , x = 1 or 2
- * ERROR => |0 -> TYPE | 1 -> ISS | 2-5 -> TSP | 6 -> ERR |
- **/
-void receiveData(char *readBuf)
-{
-
-    int readed = 0;
-    while (1)
-    {
-        for (int i = 0; i < configuredPorts; i++)
-        {
-            readed = RS232_PollComport(actualConfig[i].serialNumber, readBuf, SIZE3);
-
-            if (readed > 0)
-            {
-                if (readBuf[0] >= ERROR && readBuf[0] <= DATA2)
-                {
-
-                    actualConfig[i].iss = readBuf[1];
-                    uint32_t timestamp = join32(readBuf + 2);
-
-                    uint8_t type = readBuf[6];
-
-                    if (readBuf[0] == ERROR)
-                    {
-                        printf("ERROR =>  ISS: %u, TIMESTAMP: %u, ERRO: %u\n", actualConfig[i].iss, timestamp, type);
-                    }
-                    else
-                    {
-                        for (int j = 7; j < readed; j+4)
-                        {   
-                            float value = joinFloat(readBuf + j);
-                            printf("DATA =>  ISS: %u, TIMESTAMP: %u, TIPO: %u, VALOR: %f\n", actualConfig[i].iss, timestamp, type, value);
-                        }
-                    }
-                }
-            }
-            usleep(100000); /* sleep for 100 milliSeconds */
-        }
-    }
-}
-
 /**
  * @param sig - signal identifier
  * 
@@ -103,62 +43,20 @@ void handler(int sig)
     }
     //close all
     closeFiles();
-    //comTerminate();
 }
 
-/**
-* close all files and com ports
-**/
-void closeFiles()
+int main()
 {
-    close(fdLogs);
-    close(fdErrors);
-    close(fdData);
-    for(int i=0; i<configuredPorts;i++){
-        if(actualConfig[i].opened){
-            RS232_CloseComport(actualConfig[i].serialNumber);
-        }
-    }
-}
+    signal(SIGINT, handler);
+    configuredPorts = 0;
 
-/**
- * @param str - buffer to write on serial port to arduino
- * 
- * this function handles the starter packet
- **/
-void handleBegin(char *str)
-{
-    for (int i = 0; i < configuredPorts; i++)
-    {
-        if (actualConfig[i].opened)
-        {
-            printf("%s \n", str);
-            int size = buildStartPacket(str, i);
-            RS232_SendBuf(actualConfig[i].serialNumber, str, size);
-        }
-    }
-}
+    char bufWrite[1024];
+    char bufRead[1024];
 
-/*
-* function to open the config file declarated portSerial to receive
-* data from arduino
-**/
-void openSerial()
-{
-    for (int i = 0; i < configuredPorts; i++)
-    {
-        actualConfig[i].serialNumber = RS232_GetPortnr(actualConfig[i].portSerial);
-        if (actualConfig[i].serialNumber == -1)
-        {
-            actualConfig[i].serialNumber = 6;
-        }
-        int status = RS232_OpenComport(actualConfig[i].serialNumber, 115200, "8N1", 0);
-        if (status)
-        {
-            actualConfig[i].opened = status;
-            printf("Cannot open port %s :c\n", actualConfig[i].portSerial);
-        }
-    }
+    readConfigFile();
+    openSerial();
+    handleBegin(bufWrite);
+    receiveData(bufRead);
 }
 
 /**
@@ -201,6 +99,115 @@ void readConfigFile()
     }
 
     printConfig();
+}
+
+/*
+* function to open the config file declarated portSerial to receive
+* data from arduino
+**/
+void openSerial()
+{
+    for (int i = 0; i < configuredPorts; i++)
+    {
+        actualConfig[i].serialNumber = RS232_GetPortnr(actualConfig[i].portSerial);
+        if (actualConfig[i].serialNumber == -1)
+        {
+            actualConfig[i].serialNumber = 6;
+        }
+        int status = RS232_OpenComport(actualConfig[i].serialNumber, 115200, "8N1", 0);
+        if (status)
+        {
+            actualConfig[i].opened = status;
+            printf("Cannot open port %s :c\n", actualConfig[i].portSerial);
+        }
+    }
+}
+
+/**
+ * @param str - buffer to write on serial port to arduino
+ * 
+ * this function handles the starter packet and create the log files
+ **/
+void handleBegin(char *str)
+{
+    for (int i = 0; i < configuredPorts; i++)
+    {
+        if (actualConfig[i].opened)
+        {
+            printf("%s \n", str);
+            int size = buildStartPacket(str, i);
+            RS232_SendBuf(actualConfig[i].serialNumber, str, size);
+        }
+    }
+
+    fdData = open("files/fdData.csv", O_CREAT | O_APPEND);
+    fdErrors = open("files/fdErrors.txt", O_CREAT | O_APPEND);
+}
+
+/**
+ * @param readBuf - char buffer
+ * handles incoming data from sensors
+ * DATAx => |0 -> TYPE | 1 -> ISS | 2-5 -> TSP | 6 -> TGM | 7... -> VALi| , x = 1 or 2
+ * ERROR => |0 -> TYPE | 1 -> ISS | 2-5 -> TSP | 6 -> ERR |
+ **/
+void receiveData(char *readBuf)
+{
+
+    int readed = 0;
+    while (1)
+    {
+        for (int i = 0; i < configuredPorts; i++)
+        {
+            readed = RS232_PollComport(actualConfig[i].serialNumber, readBuf, SIZE3);
+
+            if (readed > 0)
+            {
+                if (readBuf[0] >= ERROR && readBuf[0] <= DATA2)
+                {
+
+                    actualConfig[i].iss = readBuf[1];
+                    uint32_t timestamp = join32(readBuf + 2);
+
+                    uint8_t type = readBuf[6];
+
+                    if (readBuf[0] == ERROR)
+                    {
+                        printf("ERROR =>  ISS: %u, TIMESTAMP: %u, ERRO: %u\n", actualConfig[i].iss, timestamp, type);
+                    }
+                    else
+                    {
+                        for (int j = 7; j < readed; j = j + 4)
+                        {
+                            float value = joinFloat(readBuf + j);
+                            char entry[SIZE1];
+                            sprintf(entry,"%u;%s;%s;%u;%c;%f\n", 
+                                actualConfig[i].iss, actualConfig[i].area, actualConfig[i].GPS, timestamp, (char) type, value);
+                            write(fdData, entry, sizeof(entry));
+                            //printf("DATA =>  ISS: %u, TIMESTAMP: %u, TIPO: %c, VALOR: %f\n", actualConfig[i].iss, timestamp, (char)type, value);
+                        }
+                    }
+                }
+            }
+            usleep(100000); /* sleep for 100 milliSeconds */
+        }
+    }
+}
+
+/**
+* close all files and com ports
+**/
+void closeFiles()
+{
+    close(fdLogs);
+    close(fdErrors);
+    close(fdData);
+    for (int i = 0; i < configuredPorts; i++)
+    {
+        if (actualConfig[i].opened)
+        {
+            RS232_CloseComport(actualConfig[i].serialNumber);
+        }
+    }
 }
 
 /**
