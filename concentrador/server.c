28bfa8d9965eb9766c4b1e5967c841a38cb84ca3
Joao Alves <jmca-2000@hotmail.com>
2021-05-03 17:59:42 +0100

    new UDP client

--- concentrador/server.c
+++ concentrador/server.c
@@ -12,32 +12,33 @@
 #define PORT_UDP    7777
 #define PORT_TCP    7778
 #define MAXLINE 1024
+struct sockaddr_in cliaddr_TCP;
+struct sockaddr_in servaddr_UDP;
   
   
-// Driver code
+//servidor UDP e cliente TCP
 int main() {
     int sockfd;
     char buffer[MAXLINE];
-    char *hello = "Hello from server";
-    struct sockaddr_in cliaddr;
+    char message[MAXLINE];
       
     // Creating socket file descriptor
     if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
-      
-    memset(&servaddr, 0, sizeof(servaddr));
-    memset(&cliaddr, 0, sizeof(cliaddr));
+
+    memset(&servaddr_UDP, 0, sizeof(servaddr_UDP));
+    memset(&cliaddr_TCP, 0, sizeof(cliaddr_TCP));
       
     // Filling server information
-    servaddr.sin_family    = AF_INET; // IPv4
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(PORT);
+    servaddr_UDP.sin_family    = AF_INET; // IPv4
+    servaddr_UDP.sin_addr.s_addr = INADDR_ANY;
+    servaddr_UDP.sin_port = htons(PORT_UDP);
       
     // Bind the socket with the server address
-    if ( bind(sockfd, (const struct sockaddr *)&servaddr, 
-            sizeof(servaddr)) < 0 )
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr_UDP, 
+            sizeof(servaddr_UDP)) < 0 )
     {
         perror("bind failed");
         exit(EXIT_FAILURE);
@@ -45,16 +46,19 @@ int main() {
       
     int len, n;
   
-    len = sizeof(cliaddr);  //len is value/resuslt
+    len = sizeof(cliaddr_TCP);  //len is value/resuslt
   
     n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
-                MSG_WAITALL, ( struct sockaddr *) &cliaddr,
+                MSG_WAITALL, ( struct sockaddr *) &cliaddr_TCP,
                 &len);
     buffer[n] = '\0';
     printf("Client : %s\n", buffer);
-    sendto(sockfd, (const char *)hello, strlen(hello), 
-        MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
-            len);
+    memset(message, 0, sizeof message);
+    sprintf(message,"1;%s",buffer);
+
+    //escrever TCP a message
+
+
     printf("Hello message sent.\n"); 
       
     return 0;
--- sensors/udp_client.c
+++ sensors/udp_client.c
@@ -11,14 +11,17 @@
 #include <fcntl.h>
 #include "../concentrador/api.h"
 
-#define PORT_UDP    7777
-#define PORT_TCP    7778
+#define PORT_UDP 7777
+#define PORT_TCP 7778
 #define MAXLINE 1024
-  
+int sockfd;
+struct sockaddr_in servaddr;
 
-int openFile(){
+int openFile()
+{
     int fd = open("../concentrador/log/data.csv", O_RDONLY);
-    if(fd < 0){
+    if (fd < 0)
+    {
         write(1, "error opening data file", 24);
         _exit(0);
     }
@@ -26,51 +29,47 @@ int openFile(){
     return fd;
 }
 
-void handleData(){
-    while (1)
+void handleData(int fd)
+{
+    int n;
+    char buf[1024];
+    while ((n = read(fd, buf, sizeof(buf))) > 0)
     {
-        
+        char *token = strtok(buf, "\n");
+        while (token != NULL)
+        {
+            sendto(sockfd, token, strlen(token),
+                   MSG_CONFIRM, (const struct sockaddr *)&servaddr,
+                   sizeof(servaddr));
+            printf("Hello message sent.\n");
+            sleep(2);
+            token = strtok(NULL, "\n");
+        }
     }
-    
-
-
 }
 
-
 // Driver code
-int main() {
-    int sockfd;
+int main()
+{
+    
     char buffer[MAXLINE];
     int fd = openFile();
-    
-    struct sockaddr_in     servaddr;
-  
+
     // Creating socket file descriptor
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
-  
+
     memset(&servaddr, 0, sizeof(servaddr));
-      
+
     // Filling server information
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT);
+    servaddr.sin_port = htons(PORT_UDP);
     servaddr.sin_addr.s_addr = INADDR_ANY;
-      
-    int n, len;
-      
-    sendto(sockfd, (const char *)hello, strlen(hello),
-        MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
-            sizeof(servaddr));
-    printf("Hello message sent.\n");
-          
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
-                MSG_WAITALL, (struct sockaddr *) &servaddr,
-                &len);
-    buffer[n] = '\0';
-    printf("Server : %s\n", buffer);
-  
+
+    handleData(fd);
     close(sockfd);
     return 0;
 }
