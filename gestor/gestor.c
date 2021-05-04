eec40a6b9672fda1349d792291ee7b9e44919282
Joao Alves <jmca-2000@hotmail.com>
2021-05-04 20:04:36 +0100

    Identation and comments

--- gestor/gestor.c
+++ gestor/gestor.c
@@ -1,4 +1,4 @@
-// Server side implementation of UDP client-server model
+// Server side implementation of TCP client-server model
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,39 +14,19 @@
 #define MAXLINE 1024
 #define MAX 127
 
-void func(int sockfd, int fd)
-{
-    char buff[MAX];
-    char aux[MAX];
-    int n;
-    // infinite loop for chat
-    for (;;) {
-        bzero(buff, MAX);
-        bzero(aux, MAX);
-        // read the message from client and copy it in buffer
-        read(sockfd, buff, sizeof(buff));
-        // print buffer which contains the client contents
-        printf("From client: %s\n ", buff);
-        sprintf(aux, "%s\n",buff);
-        write(fd, aux, strlen(aux));
-  
-        // if msg contains "Exit" then server exit and chat ended.
-        if (strncmp("exit", buff, 4) == 0) {
-            printf("Server Exit...\n");
-            break;
-        }
-    }
-}
-  
-// Driver function
+  void func(int sockfd, int fd);
+
+
 int main()
 {
     int sockfd, connfd, len;
     struct sockaddr_in servaddr, cli;
+
     int fd = open("../db/data.txt", O_RDWR | O_CREAT | O_APPEND, 0666);
   
     // socket create and verification
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
     if (sockfd == -1) {
         printf("socket creation failed...\n");
         exit(0);
@@ -60,7 +40,7 @@ int main()
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(PORT_TCP);
   
-    // Binding newly created socket to given IP and verification
+    // Binding created socket to given IP and verification
     if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
         printf("socket bind failed...\n");
         exit(0);
@@ -68,17 +48,20 @@ int main()
     else
         printf("Socket successfully binded..\n");
   
-    // Now server is ready to listen and verification
+
+    // Server is ready to listen and verification
     if ((listen(sockfd, 5)) != 0) {
         printf("Listen failed...\n");
         exit(0);
     }
     else
         printf("Server listening..\n");
+        
     len = sizeof(cli);
   
     // Accept the data packet from client and verification
     connfd = accept(sockfd, (SA*)&cli, &len);
+
     if (connfd < 0) {
         printf("server acccept failed...\n");
         exit(0);
@@ -86,10 +69,39 @@ int main()
     else
         printf("server acccept the client...\n");
   
+
     // Function for chatting between client and server
     func(connfd, fd);
   
     // After chatting close the socket
     close(sockfd);
     close(fd);
+}
+
+
+void func(int sockfd, int fd)
+{
+    char buff[MAX];
+    char aux[MAX];
+
+    while (1) {
+        bzero(buff, MAX);
+        bzero(aux, MAX);
+
+        // read the message from client and copy it in buffer
+        read(sockfd, buff, sizeof(buff));
+
+        // print from  the client's buffer contents
+        printf("From client: %s\n ", buff);
+
+        //writing the content recieved from the client to the database file
+        sprintf(aux, "%s\n",buff);
+        write(fd, aux, strlen(aux));
+  
+        // if msg contains "Exit" then server exit and chat ended(needs to change)
+        if (strncmp("exit", buff, 4) == 0) {
+            printf("Server Exit...\n");
+            break;
+        }
+    }
 }
