bf5bc27bdde53c27dc0fbd1e9f8fd9670aebcf50
Joao Alves <jmca-2000@hotmail.com>
2021-05-06 01:54:03 +0100

    Threads

--- gestor/gestor.c
+++ gestor/gestor.c
@@ -9,13 +9,18 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <pthread.h>
 #define SA struct sockaddr
-#define PORT_TCP    7778
+#define PORT_TCP 7778
 #define MAXLINE 1024
 #define MAX 127
 
-  void func(int sockfd, int fd);
+struct threadParam {
+    int sockfd, fd;
 
+};
+
+void *func(void* arg);
 
 int main()
 {
@@ -23,83 +28,92 @@ int main()
     struct sockaddr_in servaddr, cli;
 
     int fd = open("../db/data.txt", O_RDWR | O_CREAT | O_APPEND, 0666);
-  
+
     // socket create and verification
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (sockfd == -1) {
+    if (sockfd == -1)
+    {
         printf("socket creation failed...\n");
         exit(0);
     }
     else
         printf("Socket successfully created..\n");
     bzero(&servaddr, sizeof(servaddr));
-  
+
     // assign IP, PORT
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(PORT_TCP);
-  
+
     // Binding created socket to given IP and verification
-    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
+    if ((bind(sockfd, (SA *)&servaddr, sizeof(servaddr))) != 0)
+    {
         printf("socket bind failed...\n");
         exit(0);
     }
     else
         printf("Socket successfully binded..\n");
-  
 
     // Server is ready to listen and verification
-    if ((listen(sockfd, 5)) != 0) {
+    if ((listen(sockfd, 5)) != 0)
+    {
         printf("Listen failed...\n");
         exit(0);
     }
     else
         printf("Server listening..\n");
-        
+
     len = sizeof(cli);
-  
+
     // Accept the data packet from client and verification
-    connfd = accept(sockfd, (SA*)&cli, &len);
+    while (1)
+    {
+        pthread_t tid;
+        connfd = accept(sockfd, (SA *)&cli, &len);
 
-    if (connfd < 0) {
-        printf("server acccept failed...\n");
-        exit(0);
+        if (connfd < 0)
+        {
+            printf("server acccept failed...\n");
+        }
+        else
+            printf("server acccept the client...\n");
+        struct threadParam param;
+        param.fd=fd;
+        param.sockfd=sockfd;
+        pthread_create(&tid,NULL,func,&param);
     }
-    else
-        printf("server acccept the client...\n");
-  
 
-    // Function for chatting between client and server
-    func(connfd, fd);
-  
+
     // After chatting close the socket
     close(sockfd);
     close(fd);
 }
 
-
-void func(int sockfd, int fd)
+void *func(void* arg)
 {
+    struct threadParam *param= arg;
     char buff[MAX];
     char aux[MAX];
 
-    while (1) {
+    while (1)
+    {
         bzero(buff, MAX);
         bzero(aux, MAX);
 
         // read the message from client and copy it in buffer
-        read(sockfd, buff, sizeof(buff));
+        read(param->sockfd, buff, sizeof(buff));
 
         // print from  the client's buffer contents
-        printf("From client: %s\n ", buff);
+        printf("%s\n ", buff);
 
         //writing the content recieved from the client to the database file
-        sprintf(aux, "%s\n",buff);
-        write(fd, aux, strlen(aux));
-  
+        sprintf(aux, "%s\n", buff);
+        write(param->fd, aux, strlen(aux));
+
         // if msg contains "Exit" then server exit and chat ended(needs to change)
-        if (strncmp("exit", buff, 4) == 0) {
+        if (strncmp("exit", buff, 4) == 0)
+        {
             printf("Server Exit...\n");
             break;
         }
